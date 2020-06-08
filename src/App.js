4d7b88d48c9451dc0992ec469182b49cd66ff51c
mahadalifazil <37039042+mahadalifazil@users.noreply.github.com>
2020-06-08 15:28:31 +0500

    updated version 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>mahad is a good boy (react app) version 1</h1>
+      <h1>mahad is a good boy (react app) version 2</h1>
     </div>
   );
 }
